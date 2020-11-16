e6de7a93fc8605f803e69bc302ae40776efd95cf
amm4677 <amm4677.g.rit.edu>
2020-11-15 21:54:52 -0500

    Added Icon

--- src/View/MyFrame.java
+++ src/View/MyFrame.java
@@ -5,6 +5,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 
